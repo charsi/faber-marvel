998f0d88cb24835925bb0733d39a99b5a127d135
Nishil <nishilfoo@gmail.com>
2018-04-17 09:41:12 -0700

    implement search

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -10,32 +10,46 @@ export class HomePage {
 
   charactersData : any;
   offset : number;
+  nameInput:string;
 
   constructor(public navCtrl: NavController, private marvelProvider:MarvelProvider) {
-    this.offset=0;
+    this.offset = 0;
+    this.charactersData = {results:[]};
   }
 
   ionViewWillEnter(){
-    this.marvelProvider.getCharacters(0).subscribe(
-      (res)=>{
-        this.charactersData = res.data;
-        console.log(this.charactersData);
-      });
+    this.getData();
   }
   
   doInfinite(scroll){
-    
-    this.marvelProvider.getCharacters(this.offset+20).subscribe(
+    return this.marvelProvider.getCharacters(this.offset+20, this.nameInput).subscribe(
       (res)=>{
-        console.log(res);
         this.charactersData.results.push.apply(this.charactersData.results, res.data.results );
-        console.log(this.charactersData);
-        this
         this.offset += 20;
         scroll.complete();
       });
     
-    
+  }
+
+  search(ev){
+    this.offset = 0;
+    this.charactersData = {results:[]};
+    this.nameInput = ev.target.value;
+    this.getData();
+  }
+
+  cancel(){
+    this.offset = 0;
+    this.charactersData = {results:[]};
+    this.getData();
+  }
+
+  getData(){
+    return this.marvelProvider.getCharacters(this.offset, this.nameInput).subscribe(
+      (res)=>{
+        this.charactersData.results.push.apply(this.charactersData.results, res.data.results );
+        console.log(this.charactersData, this.nameInput);
+      });
   }
 
 }
--- src/providers/marvel/marvel.ts
+++ src/providers/marvel/marvel.ts
@@ -33,14 +33,15 @@ export class MarvelProvider {
   }
 
 
-  getCharacters(offset:number){
+  getCharacters(offset:number, startsWith?:string){
     var ts : string = new Date().toString();
-    var params = {
+    var params : any = {
       ts : ts,
       apikey : this.apiPubKey,
       hash: this.apiToken(ts),
       offset: offset.toString()
     };
+    if(startsWith){params.nameStartsWith = startsWith;}
     return this.http.get(this.url+'characters', { params:params });
   }
 }
